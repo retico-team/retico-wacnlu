77d1141b106793089595e568b449724547113967
Casey Kennington <bakuzen@gmail.com>
2023-10-18 07:19:22 -0600

    updated WAC module; not tested

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # retico-wacnlu
 Words-as-Classifiers model as reference resolution / NLU module
+
+### Installation and Requirements
+
+### Example
+
+```python
+import sys
+from retico import *
+
+prefix = '/path/to/modules/'
+sys.path.append(prefix+'retico-clip')
+sys.path.append(prefix+'retico-vision')
+sys.path.append(prefix+'retico-yolov8')
+
+# from retico_yolov8 import YoloV8
+from retico_core.audio import MicrophoneModule
+from retico_googleasr.googleasr import GoogleASRModule
+from retico_core.text import IncrementalizeASRModule
+from retico_clip.clip import ClipObjectFeatures
+from retico_vision.vision import WebcamModule 
+from retico_yolov8.yolov8 import Yolov8
+from retico_wacnlu.words_as_classifiers import 
+
+wac_dir = 'path/to/wac/classifiers'
+
+mic = MicrophoneModule(1000)
+asr = GoogleASRModule()
+iasr = IncrementalizeASRModule()
+webcam = WebcamModule()
+yolo = Yolov8()
+feats = ClipObjectFeatures(show=True)
+wac = WordsAsClassifiersModule(wac_dir=wac_dir)
+debug = modules.DebugModule()
+
+webcam.subscribe(yolo)
+yolo.subscribe(feats)
+feats.subscribe(wac)
+asr.subscribe(iasr)
+iasr.subscribe(wac)
+wac.subscribe(debug)
+
+mic.run()
+asr.run()
+wac.run()
+iasr.run()
+debug.run()
+
+input()
+
+mic.stop()
+asr.stop()
+iasr.stop()
+wac.stop()
+debug.stop()
+```
+
+### Citation
+
+```
+
+@inproceedings{kennington-schlangen-2015-simple,
+    title = "Simple Learning and Compositional Application of Perceptually Grounded Word Meanings for Incremental Reference Resolution",
+    author = "Kennington, Casey  and
+      Schlangen, David",
+    booktitle = "Proceedings of the 53rd Annual Meeting of the Association for Computational Linguistics and the 7th International Joint Conference on Natural Language Processing (Volume 1: Long Papers)",
+    month = jul,
+    year = "2015",
+    address = "Beijing, China",
+    publisher = "Association for Computational Linguistics",
+    url = "https://aclanthology.org/P15-1029",
+    doi = "10.3115/v1/P15-1029",
+    pages = "292--301",
+}
+
+```
---
+++ reitco_wacnlu/words_as_classifiers.py
@@ -0,0 +1,109 @@
+"""A module for grounded semantics"""
+
+# retico
+from retico_core import abstract
+from retico_vision.vision import ObjectFeaturesIU
+from retico_core.text.common import SpeechRecognitionIU
+from reitco_wacnlu.wac import WAC
+from reitco_wacnlu.common import GroundedFrameIU
+
+from collections import deque
+import numpy as np
+import os
+import os.path as osp
+from tqdm import tqdm
+import sys
+import threading
+import time
+
+
+class WordsAsClassifiersModule(abstract.AbstractModule):
+    """A model of grounded semantics. 
+
+    Attributes:
+        model_dir(str): directory where WAC classifiers are saved
+    """
+
+    @staticmethod
+    def name():
+        return "SLIM Group WAC Model"
+
+    @staticmethod
+    def description():
+        return "WAC Visually-Grounded Model"
+
+    @staticmethod
+    def input_ius():
+        return [ObjectFeaturesIU,SpeechRecognitionIU]
+
+    @staticmethod
+    def output_iu():
+        return GroundedFrameIU
+
+    def __init__(self, wac_dir, **kwargs):
+        """Loads the WAC models.
+
+        Args:
+            model_dir (str): The path to the directory of WAC classifiers saved as pkl files.
+        """
+        super().__init__(**kwargs)
+        self.wac = WAC(wac_dir)
+        self.word_buffer = None
+        self.itts = 0
+        self.train_mode = False
+        self.queue = deque(maxlen=1)
+
+    def train_wac(self):
+        wac = self.wac.copy()
+        print('updating negatives')
+        wac.create_negatives()
+        print('training')
+        wac.train()
+        print('persisting')
+        wac.persist_model()
+
+
+    def process_iu(self, input_iu):
+        frame = {}
+        # print(input_iu)
+        if isinstance(input_iu, SpeechRecognitionIU):
+            self.word_buffer = input_iu.get_text().lower().split()[-1]
+            if not self.train_mode:
+                frame['word_to_find'] = self.word_buffer
+        
+        # when new objects are observed (i.e., not SpeechRecognitionIUs)
+        if isinstance(input_iu, ObjectFeaturesIU):
+            objects = input_iu.payload
+            if objects is None: return
+            # WAC wants a list of intents (objectIDs) and their corresponding features in a tuple
+            if len(objects) == 0: return
+            intents = objects.keys()
+            features = [np.array(objects[obj_id]) for obj_id in objects]
+            
+            if not self.train_mode:
+                word,_ = self.wac.best_word((intents, features))
+                frame['best_known_word'] = word
+            
+            if self.train_mode:
+                if self.word_buffer is not None:
+                    self.wac.add_positive_observation(self.word_buffer, features[0])
+                    self.itts += 1
+                    if self.itts % 100 == 0:
+                        t = threading.Thread(target=self.train_wac)
+                        t.start()
+
+            if self.word_buffer is not None:
+                target = self.wac.best_object(self.word_buffer, (intents, features))
+                if target is not None: 
+                    frame['best_object'] = target[0] 
+                    frame['obj_confidence'] = target[1] 
+
+        if len(frame) == 0: return
+        output_iu = self.create_iu(input_iu)
+        output_iu.set_frame(frame)
+        self.append(output_iu)
+
+
+    def setup(self):
+        if not self.train_mode:
+            self.wac.load_model()
