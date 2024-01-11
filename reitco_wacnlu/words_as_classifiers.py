4eecc200adc0703fa659beec5c150dff0e690255
Drew McMains <andrewmcmains@u.boisestate.edu>
2024-01-11 11:04:26 -0700

    rrsds update (WIP)

--- reitco_wacnlu/words_as_classifiers.py
+++ reitco_wacnlu/words_as_classifiers.py
@@ -1,23 +1,19 @@
 """A module for grounded semantics"""
 
 # retico
-from retico_core import abstract
+import retico_core
 from retico_vision.vision import ObjectFeaturesIU
-from retico_core.text.common import SpeechRecognitionIU
-from reitco_wacnlu.wac import WAC
-from reitco_wacnlu.common import GroundedFrameIU
+from retico_core.text import SpeechRecognitionIU
+from retico_wacnlu.wac import WAC
+from retico_wacnlu.common import GroundedFrameIU
 
 from collections import deque
 import numpy as np
-import os
 import os.path as osp
 from tqdm import tqdm
-import sys
 import threading
-import time
 
-
-class WordsAsClassifiersModule(abstract.AbstractModule):
+class WordsAsClassifiersModule(retico_core.AbstractModule):
     """A model of grounded semantics. 
 
     Attributes:
@@ -63,46 +59,54 @@ class WordsAsClassifiersModule(abstract.AbstractModule):
         wac.persist_model()
 
 
-    def process_iu(self, input_iu):
-        frame = {}
-        # print(input_iu)
-        if isinstance(input_iu, SpeechRecognitionIU):
-            self.word_buffer = input_iu.get_text().lower().split()[-1]
-            if not self.train_mode:
-                frame['word_to_find'] = self.word_buffer
-        
-        # when new objects are observed (i.e., not SpeechRecognitionIUs)
-        if isinstance(input_iu, ObjectFeaturesIU):
-            objects = input_iu.payload
-            if objects is None: return
-            # WAC wants a list of intents (objectIDs) and their corresponding features in a tuple
-            if len(objects) == 0: return
-            intents = objects.keys()
-            features = [np.array(objects[obj_id]) for obj_id in objects]
+    def process_update(self, update_message):
+        for iu, ut in update_message:
+            if ut != retico_core.UpdateType.ADD:
+                continue
+            frame = {}
+            # print(iu)
+            if isinstance(iu, SpeechRecognitionIU):
+                print(iu.get_text())
+                if iu.get_text() == '': continue
+                self.word_buffer = iu.get_text().lower().split()[-1]
+                if not self.train_mode:
+                    frame['word_to_find'] = self.word_buffer
             
-            if not self.train_mode:
-                word,_ = self.wac.best_word((intents, features))
-                frame['best_known_word'] = word
-            
-            if self.train_mode:
+            # when new objects are observed (i.e., not SpeechRecognitionIUs)
+            if isinstance(iu, ObjectFeaturesIU):
+                objects = iu.payload
+                if objects is None: return
+                # WAC wants a list of intents (objectIDs) and their corresponding features in a tuple
+                if len(objects) == 0: return
+                intents = objects.keys()
+                features = [np.array(objects[obj_id]) for obj_id in objects]
+                
+                if not self.train_mode:
+                    word,_ = self.wac.best_word((intents, features[0]))
+                    print("best word for object:", word)
+                    frame['best_known_word'] = word
+                
+                if self.train_mode:
+                    if self.word_buffer is not None:
+                        self.wac.add_positive_observation(self.word_buffer, features[0])
+                        self.itts += 1
+                        if self.itts % 2 == 0:
+                            t = threading.Thread(target=self.train_wac)
+                            t.start()
+
                 if self.word_buffer is not None:
-                    self.wac.add_positive_observation(self.word_buffer, features[0])
-                    self.itts += 1
-                    if self.itts % 100 == 0:
-                        t = threading.Thread(target=self.train_wac)
-                        t.start()
+                    target = self.wac.best_object(self.word_buffer, (intents, features))
+                    if target is not None: 
+                        print('best object', target)
+                        frame['best_object'] = target[0] 
+                        frame['obj_confidence'] = target[1] 
 
-            if self.word_buffer is not None:
-                target = self.wac.best_object(self.word_buffer, (intents, features))
-                if target is not None: 
-                    frame['best_object'] = target[0] 
-                    frame['obj_confidence'] = target[1] 
-
-        if len(frame) == 0: return
-        output_iu = self.create_iu(input_iu)
-        output_iu.set_frame(frame)
-        self.append(output_iu)
-
+            if len(frame) == 0: return
+            output_iu = self.create_iu(iu)
+            output_iu.set_frame(frame)
+            output_iu = retico_core.UpdateMessage.from_iu(output_iu, retico_core.UpdateType.ADD)
+            self.append(output_iu)
+            
 
     def setup(self):
         if not self.train_mode:
